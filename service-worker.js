048d2157db84e0c81ed237834575e18f002e2b54
squipan <smurfjpn111@gmail.com>
2026-06-28 01:03:20 +0900

    Bump service worker cache version to v9 to force refresh

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Enables offline use + PWA installability
 // =============================================
 
-const CACHE_NAME = 'oinar-v8';
+const CACHE_NAME = 'oinar-v9';
 
 // All files to cache for offline use
 const ASSETS_TO_CACHE = [
