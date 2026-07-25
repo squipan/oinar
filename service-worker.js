7439f87427c7fd4cb1126425b65602dd57b2037a
squipan <smurfjpn111@gmail.com>
2026-07-26 00:24:50 +0900

    Bump cache version to oinar-v26 and force push update for Profits Calendar

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Enables offline use + PWA installability
 // =============================================
 
-const CACHE_NAME = 'oinar-v25';
+const CACHE_NAME = 'oinar-v26';
 
 // All files to cache for offline use
 const ASSETS_TO_CACHE = [
